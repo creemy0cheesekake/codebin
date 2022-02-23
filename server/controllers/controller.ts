942088eba3d17470a23e452ec329b37f469ebf0d
Arjun Anand <arjunanand1405@gmail.com>
2022-02-22 22:24:54 -0600

    functions good for server controller

--- server/controllers/controller.ts
+++ server/controllers/controller.ts
@@ -1,13 +1,10 @@
 import { Request, Response } from "express";
 import { generateLink, hashPassword, comparePassword } from "./helperFunctions";
 import Schema from "../schemas/Schema";
-import { Model } from "mongoose";
 
 export const createNewEntry = async (req: Request, res: Response) => {
 	try {
 		let link = generateLink();
-		while (!!(await Schema.findOne({ link: { $eq: link } })))
-			link = generateLink();
 		const { body } = req.body;
 		await Schema.create({
 			link,
@@ -33,11 +30,9 @@ export const updateEntry = async (req: Request, res: Response) => {
 
 		const entry = await Schema.findOne({ link: { $eq: link } });
 
-		// if password is null it will delete password. if password is undefined then it wont change
-		entry.password =
-			password !== undefined
-				? await hashPassword(password)
-				: entry.password;
+		if (password !== undefined)
+			entry.password = await hashPassword(password);
+
 		entry.body = body;
 		entry.save();
 
@@ -80,9 +75,7 @@ export const getEntry = async (req: Request, res: Response) => {
 	try {
 		const { link } = req.params;
 
-		const entry: Model<any> | null = await Schema.findOne({
-			link: { $eq: link },
-		});
+		const entry = await Schema.findOne({ link: { $eq: link } });
 
 		res.json({
 			success: true,
--- server/controllers/helperFunctions.ts
+++ server/controllers/helperFunctions.ts
@@ -1,11 +1,13 @@
 import bcrypt from "bcrypt";
+import Schema from "../schemas/Schema";
 
 const randomLinkChars =
 	"1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
-export const generateLink = () => {
+export const generateLink: any = async () => {
 	let res = "";
 	for (let i = 0; i < 6; i++)
 		res += randomLinkChars.charAt(Math.random() * randomLinkChars.length);
+	if (!!(await Schema.findOne({ link: { $eq: res } }))) return generateLink();
 	return res;
 };
 
