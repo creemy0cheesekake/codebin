5419b262cbcd1ca4456bb16b49f95f07c8192f74
Arjun Anand <arjunanand1405@gmail.com>
2022-03-07 16:52:17 -0600

    ready to deploy

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,16 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/globals.sass";
 import ContextProvider from "./components/Context";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
 	<React.StrictMode>
 		<ContextProvider>
-			<Router>
-				<Routes>
-					<Route path="*" element={<App />} />
-				</Routes>
-			</Router>
+			<App />
 		</ContextProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
